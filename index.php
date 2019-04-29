9c69408751a411518dfe86247d0d9062ceeb5abc
Evan Wheeler <effyn@gmx.com>
2019-04-29 01:39:53 -0400

    A lot of comments

--- index.php
+++ index.php
@@ -7,22 +7,30 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+// start up f3
 require_once('vendor/autoload.php');
 
+// start up a session
 session_start();
+
+// create an instance and enable debugging
 $f3 = Base::instance();
 $f3->set('DEBUG', 3);
 
+
+// root route, shows the landing page
 $f3->route('GET /', function() {
     $view = new Template();
     echo $view->render('views/home.html');
 });
 
+// 1st of three forms
 $f3->route('GET /join', function() {
     $view = new Template();
     echo $view->render('views/form1.html');
 });
 
+// 2nd of three forms, collects data from form 1 in session
 $f3->route('POST /profile', function() {
     $_SESSION['firstName'] = $_POST['firstName'];
     $_SESSION['lastName'] = $_POST['lastName'];
@@ -34,6 +42,7 @@ $f3->route('POST /profile', function() {
     echo $view->render('views/form2.html');
 });
 
+// 3rd of three forms, collects data from form 2 in session
 $f3->route('POST /interests', function() {
     $_SESSION['email'] = $_POST['email'];
     $_SESSION['state'] = $_POST['state'];
@@ -44,6 +53,7 @@ $f3->route('POST /interests', function() {
     echo $view->render('views/form3.html');
 });
 
+// final page, combines interests and concatenates elements to a string
 $f3->route('POST /summary', function() {
 
     $indoor = isset($_POST['indoor']) ? $_POST['indoor'] : [];
@@ -60,5 +70,5 @@ $f3->route('POST /summary', function() {
     echo $view->render('views/summary.html');
 });
 
-
+// run f3
 $f3 -> run();
