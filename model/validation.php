05f42fc9814435cd9494aab69f0070b5e6d8a5e6
Evan Wheeler <effyn@gmx.com>
2019-05-13 02:25:51 -0400

    Add validation functions

---
+++ model/validation.php
@@ -0,0 +1,60 @@
+<?php
+function validName($name)
+{
+    return ctype_alpha($name);
+}
+
+function validAge($age)
+{
+    if (is_numeric($age))
+    {
+        $numAge = intval($age);
+        return $numAge >= 18 && $numAge <= 118;
+    }
+
+    return false;
+}
+
+function validPhone($phone)
+{
+    return is_numeric($phone) && strlen($phone) == 10;
+}
+
+function validEmail($email)
+{
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+define('indoor', [
+    'tv', 'movies', 'cooking', 'board-games',
+    'puzzles', 'reading', 'playing-cards', 'video-games']);
+
+function validIndoor($indoor)
+{
+    foreach ($indoor as $interest)
+    {
+        if (!in_array($interest, $indoor))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+define('outdoor', [
+    'hiking', 'biking', 'swimming',
+    'collecting', 'walking', 'climbing']);
+
+function validOutdoor($outdoor)
+{
+    foreach ($outdoor as $interest)
+    {
+        if (!in_array($interest, $outdoor))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
